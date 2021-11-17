8b5567ad9afb2ee32ce0c9880a99f582f0e05a2a
シュムコヴデニス <shumkovdd@gmail.com>
2021-11-18 04:09:22 +0900

    Created simple demo

--- Engine11/MyGame.cpp
+++ Engine11/MyGame.cpp
@@ -8,15 +8,24 @@ void EngineClass::UpdateScene(double time)
 		rot = 0;
 	XMVECTOR rotaxis = XMVectorSet(0.0f, 1.0f, 0.0f, 1.0f);
 
+	//中心にある立方体	ピクサークリッピングに設定しています。
 	cube1World = XMMatrixIdentity();
 	translation = XMMatrixTranslation(0.0f, 0.0f, 4.0f);
 	rotation = XMMatrixRotationAxis(rotaxis, rot);
 	cube1World = translation * rotation;
-
+	//Y軸を回転している	透明に設定しています。
 	cube2World = XMMatrixIdentity();
 	rotation = XMMatrixRotationAxis(rotaxis, -rot);
 	scaling = XMMatrixScaling(1.3f, 1.3f, 1.3f);
 	cube2World = scaling * rotation;
+
+	rotaxis = XMVectorSet(1.0f, 0.0f, 0.0f, 1.0f);
+	//Z軸を回転している	不透明に設定しています。
+	cube3World = XMMatrixIdentity();
+	translation = XMMatrixTranslation(0.0f, 4.0f, 0.0f);
+	rotation = XMMatrixRotationAxis(rotaxis, rot);
+	cube3World = translation * rotation;
+
 
 }
 
@@ -28,12 +37,17 @@ void EngineClass::Render()
 
 
 
-	float color[] = { 0,0,0,1 };
-	float blendFactor[] = { 1.0f,1.0f,1.0f,1.0f };
+	float color[] = { 0.1f,0.1f,0.1f,1 };
+	float blendFactor[] = { 0.75f,0.75f,0.75f,1.0f };
 	devcon->ClearRenderTargetView(rtv.Get(), color);
+	//不透明
+	devcon->OMSetBlendState(0, 0, 0xffffffff);
+
 	devcon->ClearDepthStencilView(depthStencil.Get(), D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
 	
 
+		//Z軸を回転している	不透明に設定しています。
+		devcon->RSSetState(0);
 
 		WVP = cube1World * camView * camProjection;
 		cbPerObject.WVP = XMMatrixTranspose(WVP);
@@ -44,18 +58,38 @@ void EngineClass::Render()
 		devcon->PSSetSamplers(0, 1, texSamplerState.GetAddressOf());
 		devcon->DrawIndexed(36, 0, 0);
 
-
+		//中心にある立方体	ピクサークリッピングに設定しています。
+		
+		devcon->RSSetState(NoCull.Get());
 
 		WVP = cube2World * camView * camProjection;
 		cbPerObject.WVP = XMMatrixTranspose(WVP);
 
 		devcon->UpdateSubresource(constantBuffer.Get(), 0, NULL, &cbPerObject, 0, 0);
 		devcon->VSSetConstantBuffers(0, 1, constantBuffer.GetAddressOf());
-		devcon->PSSetShaderResources(0, 1, resourceTexture.GetAddressOf());
+		devcon->PSSetShaderResources(0, 1, resource2Texture.GetAddressOf());
 		devcon->PSSetSamplers(0, 1, texSamplerState.GetAddressOf());
 		devcon->DrawIndexed(36, 0, 0);
-	
 
+
+		//Y軸を回転している	透明に設定しています。
+		devcon->OMSetBlendState(transparent.Get(), blendFactor, 0xffffffff);
+
+		WVP = cube3World * camView * camProjection;
+		cbPerObject.WVP = XMMatrixTranspose(WVP);
+
+		devcon->UpdateSubresource(constantBuffer.Get(), 0, NULL, &cbPerObject, 0, 0);
+		devcon->VSSetConstantBuffers(0, 1, constantBuffer.GetAddressOf());
+		devcon->PSSetShaderResources(0, 1, resourceTexture.GetAddressOf());
+		devcon->PSSetSamplers(0, 1, texSamplerState.GetAddressOf());
+		//裏面を描画してから
+		devcon->RSSetState(CCWCull.Get());
+		devcon->DrawIndexed(36, 0, 0);
+		//表面を描画します
+		devcon->RSSetState(CWCull.Get());
+		devcon->DrawIndexed(36, 0, 0);
+
+		//2Dレンダー
 	spriteBatch->Begin();
 	DrawMyText(std::to_string(fps).c_str());
 	spriteBatch->End();
